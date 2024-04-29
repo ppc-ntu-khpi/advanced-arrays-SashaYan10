21fe7e447a49f1f278af93f14f672f383e9cfcba
Oleksandr Yanush <115615844+SashaYan10@users.noreply.github.com>
2024-04-29 11:47:08 +0000

    Finish Task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-import java.util.Arrays;
-
+/**
+ * Клас для тестування роботи програми
+ */
 public class Main {
-
+/**
+ * Метод для виведення результату
+ * @param args аргументи командного рядка
+ */
     public static void main(String[] args) {
-        int [][] matrix = new int[3][3];
-        int x=0;
+        int rows = 3;
+        int cols = 3;
 
-        for(int[] row:matrix)
-            Arrays.fill(row,x);
+        //Генерування випадкової матриці
+        int[][] matrix = finderMaxRow.generateRandomMatrix(rows, cols);
 
-        for(int[] row:matrix)
-            System.out.println(Arrays.toString(row));
+        System.out.println("Матриця:");
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+
+        //Знаходження рядка з максимальною сумою елементів у матриці
+        int maxSumRow = finderMaxRow.findMaxSumRow(matrix);
+
+        if (maxSumRow != -1) {
+            System.out.println("Рядок з максимальною сумою елементів: " + maxSumRow);
+        } else {
+            System.out.println("Матриця пуста.");
+        }
     }
 }
